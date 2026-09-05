d0ae06d69ad50d2261b44956c56022af71dd66ed
Sumit <officially.sumit.upadhyay@gmail.com>
2026-09-06 03:01:36 +0530

    Almost done with crud application

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional 
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    publish: bool = True
+    rating: Optional[int] = None
+
 
 @app.get("/")
 def root():
     return {"message": "Sumit is a monster"} 
 
-@app.get('/post')
+@app.get('/posts')
 def get_post():
     return { "data": "Your posts will be visible here"}
 
-@app.post('/create_post')
-def create_post(information: dict = Body(...)):
-    print(information)
-    # return {"data": f"{information['title']} and {information['content']}"}
+@app.post('/posts')
+def create_post(new_post: Post):
+    print(new_post)
+    print(new_post.model_dump())
+    return {"data": "Operation successful"}
